249df2fc90c36a366c5432b74407bb8e1c3ba6ae
fmadore <frederick_madore@outlook.com>
2024-03-04 10:17:10 +0100

    updated collaborators_map.py

--- Final report/collaborators_map.py
+++ Final report/collaborators_map.py
@@ -2,8 +2,9 @@ import pandas as pd
 import folium
 import requests
 from io import BytesIO
+from folium import IFrame
 
-# URL of the Excel file on GitHub (make sure it's the raw version)
+# URL of the Excel file on GitHub
 excel_url = 'https://github.com/fmadore/Remoboko/raw/master/Final%20report/Report_data.xlsx'
 
 # Fetch the content of the Excel file
@@ -13,26 +14,32 @@ data = BytesIO(response.content)
 # Load the data into a Pandas DataFrame
 df = pd.read_excel(data, sheet_name='Collaborators')
 
+# Group the data by affiliation to aggregate collaborators
+grouped = df.groupby('Affiliation')
+
 # Create the map, centered around a default location
-# You might want to change the initial location and zoom_start to better suit your data
 map = folium.Map(location=[0, 0], zoom_start=2)
 
-# Function to create the popup content for each marker
-def create_popup(row):
-    return folium.Popup(f'<a href="{row["URL"]}" target="_blank">{row["Collaborator"]}</a>', parse_html=True)
-
-# Add markers to the map
-for index, row in df.iterrows():
-    # Ensure the coordinates are correctly parsed
+# Iterate over each affiliation group
+for affiliation, group in grouped:
+    # Parse the first row to get the location (assuming all rows in the group have the same location)
     try:
-        location = [float(coord) for coord in row['Coordinate location'].split(',')]
-        folium.Marker(
-            location=location,
-            popup=create_popup(row),
-            tooltip=row['Affiliation']  # Shows the affiliation when hovering over the marker
-        ).add_to(map)
+        location = [float(coord) for coord in group.iloc[0]['Coordinate location'].split(',')]
     except ValueError:
-        print(f"Skipping {row['Collaborator']} due to invalid coordinates")
+        print(f"Skipping {affiliation} due to invalid coordinates")
+        continue
+
+    # Generate HTML content for the popup
+    popup_content = '<br>'.join([f'<a href="{row["URL"]}" target="_blank">{row["Collaborator"]}</a>' for index, row in group.iterrows()])
+    iframe = IFrame(html=popup_content, width=200, height=100)
+    popup = folium.Popup(iframe, parse_html=True)
+
+    # Add a marker for the affiliation
+    folium.Marker(
+        location=location,
+        popup=popup,
+        tooltip=affiliation  # Shows the affiliation when hovering over the marker
+    ).add_to(map)
 
 # Save the map to an HTML file
 map.save('collaborators_map.html')
