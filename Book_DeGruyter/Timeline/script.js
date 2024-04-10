b297e9bdc635f0640efd8f1e48533fa19b55233d
Frédérick Madore <93156843+fmadore@users.noreply.github.com>
2024-04-10 14:42:20 +0200

    Update script.js

--- Book_DeGruyter/Timeline/script.js
+++ Book_DeGruyter/Timeline/script.js
@@ -9,16 +9,16 @@ const events = [
                 .attr("width", 600)
                 .attr("height", events.length * 50 + 100);
 
-  // Scale for the dates
+  // Define the scales and axis
   const yScale = d3.scaleTime()
                    .domain([new Date(events[0].date), new Date(events[events.length - 1].date)])
                    .range([50, events.length * 50]);
 
-  // Add the Y Axis
   const yAxis = d3.axisLeft(yScale)
                   .ticks(d3.timeYear.every(1))
                   .tickFormat(d3.timeFormat('%Y'));
 
+  // Append the axis
   svg.append("g")
      .attr("transform", "translate(100,0)")
      .call(yAxis);
@@ -34,34 +34,5 @@ const events = [
   // Tooltip div
   const tooltip = d3.select("#tooltip");
 
-  // Place events and add interactivity for tooltip
-  events.forEach(event => {
-    const yPosition = yScale(new Date(event.date));
-    
-    svg.append("circle")
-       .attr("cx", 100)
-       .attr("cy", yPosition)
-       .attr("r", 5)
-       .attr("class", "event")
-       .on("mouseover", function(e) {
-         tooltip.transition()
-                .duration(200)
-                .style("opacity", .9);
-         tooltip.html(event.event + "<br/>" + event.date)
-                .style("left", (e.pageX + 5) + "px")
-                .style("top", (e.pageY - 28) + "px");
-       })
-       .on("mouseout", function() {
-         tooltip.transition()
-                .duration(500)
-                .style("opacity", 0);
-       });
-    
-    svg.append("text")
-       .attr("x", 120)
-       .attr("y", yPosition)
-       .text(event.event)
-       .attr("text-anchor", "start")
-       .attr("alignment-baseline", "central");
-  });
-});
+  // Dropdown filter
+  const countryFilter = d
