9933755bd1a11400aae49e8f0c6bbcf4731c34ad
Frédérick Madore <93156843+fmadore@users.noreply.github.com>
2024-04-10 14:39:44 +0200

    Update script.js

--- Book_DeGruyter/Timeline/script.js
+++ Book_DeGruyter/Timeline/script.js
@@ -7,43 +7,62 @@ const events = [
 ];
 
   const svg = d3.select("#timeline").append("svg")
-                .attr("width", 1000)
-                .attr("height", 600);
+                .attr("width", 600)
+                .attr("height", events.length * 50 + 100);
 
   // Scale for the dates
-  const xScale = d3.scaleTime()
+  const yScale = d3.scaleTime()
                    .domain([new Date(events[0].date), new Date(events[events.length - 1].date)])
-                   .range([20, 980]);
+                   .range([50, events.length * 50]);
 
-  // Add the X Axis
-  const xAxis = d3.axisBottom(xScale)
+  // Add the Y Axis
+  const yAxis = d3.axisLeft(yScale)
                   .ticks(d3.timeYear.every(1))
-                  .tickFormat(d3.timeFormat('%Y'));  // Format the ticks to show only the year
+                  .tickFormat(d3.timeFormat('%Y'));
 
   svg.append("g")
-     .attr("transform", "translate(0,100)")  // Position the x-axis
-     .call(xAxis);
+     .attr("transform", "translate(100,0)")
+     .call(yAxis);
 
   // Draw the timeline
   svg.append("line")
-     .attr("x1", 20)
+     .attr("x1", 100)
      .attr("y1", 50)
-     .attr("x2", 980)
-     .attr("y2", 50)
+     .attr("x2", 100)
+     .attr("y2", events.length * 50)
      .attr("class", "line");
 
-  // Place events
-  events.forEach(event => {
-    svg.append("circle")
-       .attr("cx", xScale(new Date(event.date)))
-       .attr("cy", 50)
-       .attr("r", 5)
-       .attr("class", "event");
+  // Tooltip div
+  const tooltip = d3.select("#tooltip");
 
+  // Place events and add interactivity for tooltip
+  events.forEach(event => {
+    const yPosition = yScale(new Date(event.date));
+    
+    svg.append("circle")
+       .attr("cx", 100)
+       .attr("cy", yPosition)
+       .attr("r", 5)
+       .attr("class", "event")
+       .on("mouseover", function(e) {
+         tooltip.transition()
+                .duration(200)
+                .style("opacity", .9);
+         tooltip.html(event.event + "<br/>" + event.date)
+                .style("left", (e.pageX + 5) + "px")
+                .style("top", (e.pageY - 28) + "px");
+       })
+       .on("mouseout", function() {
+         tooltip.transition()
+                .duration(500)
+                .style("opacity", 0);
+       });
+    
     svg.append("text")
-       .attr("x", xScale(new Date(event.date)))
-       .attr("y", 80)
+       .attr("x", 120)
+       .attr("y", yPosition)
        .text(event.event)
-       .attr("text-anchor", "middle");
+       .attr("text-anchor", "start")
+       .attr("alignment-baseline", "central");
   });
 });
