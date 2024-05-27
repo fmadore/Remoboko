4d3c1b29e92c3b33423e78f4817a5555b5228f25
fmadore <frederick_madore@outlook.com>
2024-05-27 11:28:59 +0200

    Add locations outside Benin and Togo

--- Book_DeGruyter/Maps/map_locations.py
+++ Book_DeGruyter/Maps/map_locations.py
@@ -8,14 +8,14 @@ benin_locations = {
     'Oumar Ibn Khattab Mosque (ACEEMUB)': (6.41478, 2.33682),
     'Catholic Chaplaincy of the Université d’Abomey-Calavi et des grandes écoles du Bénin': (6.42431, 2.33785),
     'Saint-Dominique Cotonou convent': (6.3558124810442935, 2.421426082297773),
-    'Bon Pasteur Parish': (6.3570692871919885, 2.398772893785117),
+    'Bon Pasteur parish': (6.3570692871919885, 2.398772893785117),
     'Bâtiment C': (6.41449, 2.34240),
     'GBEEB Headquarters': (6.372156046604895, 2.4974942706506096),
     'Père Aupiais College': (6.388846482524025, 2.3474732769556033),
     'Jardin U': (6.41389, 2.34348),
     'Lycée Béhanzin': (6.479698137720823, 2.6149709960150442),
     'University of Parakou': (9.335122564235926, 2.6466825672064243),
-    'Zogbo Parish': (6.39321349885147, 2.3915586634006054),
+    'Zogbo parish': (6.39321349885147, 2.3915586634006054),
 }
 
 # Define the locations for Togo
@@ -33,6 +33,12 @@ togo_locations = {
     'Village du Bénin': (6.16674, 1.21730),
     'Lycée de Tokoin': (6.151229331563282, 1.2280969671765716),
     'University of Kara': (9.53171555198078, 1.2075454248806252),
+}
+
+# Define the locations for West Africa
+west_africa_locations = {
+    'Université Félix Houphouët-Boigny, Côte d\'Ivoire': (5.345317, -3.979144),
+    'Cheikh Anta Diop University': (14.692778, -17.451389),
 }
 
 def calculate_map_center(locations):
@@ -66,17 +72,18 @@ def create_legend_html():
     """
     return '''
     <div style="position: fixed; 
-    bottom: 50px; left: 50px; width: 150px; height: 90px; 
+    bottom: 50px; left: 50px; width: 150px; height: 110px; 
     border:2px solid grey; z-index:9999; font-size:14px;
     background-color:white; padding: 10px;
     ">&nbsp; Points of interest <br>
     &nbsp; <i class="fa fa-map-marker" style="color:blue"></i>&nbsp; Benin <br>
-    &nbsp; <i class="fa fa-map-marker" style="color:green"></i>&nbsp; Togo
+    &nbsp; <i class="fa fa-map-marker" style="color:green"></i>&nbsp; Togo <br>
+    &nbsp; <i class="fa fa-map-marker" style="color:red"></i>&nbsp; West Africa
     </div>
     '''
 
 # Combine all locations
-all_locations = {**benin_locations, **togo_locations}
+all_locations = {**benin_locations, **togo_locations, **west_africa_locations}
 
 # Calculate the center of the map
 map_center = calculate_map_center(all_locations)
@@ -84,9 +91,10 @@ map_center = calculate_map_center(all_locations)
 # Create a base map
 m = folium.Map(location=map_center, zoom_start=8)
 
-# Add markers with labels for Benin and Togo locations
+# Add markers with labels for Benin, Togo, and West Africa locations
 add_markers_with_labels(m, benin_locations, 'blue')
 add_markers_with_labels(m, togo_locations, 'green')
+add_markers_with_labels(m, west_africa_locations, 'red')
 
 # Add the legend to the map
 legend_html = create_legend_html()
