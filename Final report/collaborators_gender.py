42b55892cd96398279ca66ca5b75f6ed7cbf62e4
fmadore <frederick_madore@outlook.com>
2024-09-10 08:32:03 +0200

    Update collaborators_gender.py

--- Final report/collaborators_gender.py
+++ Final report/collaborators_gender.py
@@ -7,10 +7,10 @@ import os
 current_dir = os.path.dirname(os.path.abspath(__file__))
 
 # Construct the full path to the JSON file
-json_file_path = os.path.join(current_dir, 'Collaborators_data.json')
+data_file_path = os.path.join(current_dir, 'Data', 'Collaborators_data.json')
 
 # Load data from the JSON file
-with open(json_file_path, 'r') as file:
+with open(data_file_path, 'r') as file:
     data = json.load(file)
 
 # Create a DataFrame from the collaborators data
@@ -19,10 +19,13 @@ df = pd.DataFrame(data)
 # Count the number of collaborators by gender
 gender_counts = df['Gender'].value_counts()
 
+# Calculate the total number of collaborators
+total_collaborators = df.shape[0]
+
 # Create a pie chart to visualize the distribution of collaborators by gender
 plt.figure(figsize=(8, 8))
 plt.pie(gender_counts, labels=gender_counts.index, autopct='%1.1f%%', startangle=140)
-plt.title('Distribution of Collaborators by Gender')
+plt.title(f'Distribution of Collaborators by Gender (Total: {total_collaborators})')
 
 # Construct full path for output file
 png_output_path = os.path.join(current_dir, 'collaborators_gender.png')
